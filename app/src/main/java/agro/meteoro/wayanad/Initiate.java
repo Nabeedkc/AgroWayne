458c0ddaf5a8328fff8dbf9757cf7770b5a8e40f
PvtSec <akssofts@gmail.com>
2020-01-22 20:28:51 +0530

    Added Internet Check

---
+++ app/src/main/java/agro/meteoro/wayanad/Initiate.java
@@ -0,0 +1,37 @@
+package agro.meteoro.wayanad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class Initiate extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        hide_sys_ui.hideui(getWindow().getDecorView());     //hide navigation keys
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initiate);
+        ConnectivityStatus();
+    }
+
+    public void ConnectivityStatus()
+    {
+        ConnectivityManager net = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        if(net.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+                net.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED)
+        {
+            startActivity(new Intent(Initiate.this,Dashboard.class));
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(),"Check your Internet Connectivity",Toast.LENGTH_SHORT).show();
+        }
+    }
+}
---
+++ app/src/main/java/agro/meteoro/wayanad/hide_sys_ui.java
@@ -0,0 +1,18 @@
+package agro.meteoro.wayanad;
+
+import android.view.View;
+
+public class hide_sys_ui
+{
+    public static void hideui(View decorView)
+    {
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+}
