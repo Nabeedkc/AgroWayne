39d9821b67868f2a45a3943c6a07fbb1bab6233c
PvtSec <akssofts@gmail.com>
2020-01-26 15:22:14 +0530

    Integrating Dashboard elements

--- app/src/main/java/agro/meteoro/wayanad/Initiate.java
+++ app/src/main/java/agro/meteoro/wayanad/Initiate.java
@@ -1,38 +1,77 @@
 package agro.meteoro.wayanad;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.widget.Toast;
 
-public class Initiate extends AppCompatActivity
-{
+public class Initiate extends AppCompatActivity {
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+    LocationManager locationManager;
+    String provider;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         hide_sys_ui.hideui(getWindow().getDecorView());     //hide navigation keys
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initiate);
         ConnectivityStatus();
     }
 
-    public void ConnectivityStatus()
-    {
-        ConnectivityManager net = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
-        if(net.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
-                net.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED)
-        {
-            startActivity(new Intent(Initiate.this,Dashboard.class));
-            finish();
+    private void ConnectivityStatus() {
+        ConnectivityManager net = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (net.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+                net.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
+            LocationPermission();
+        } else {
+            Toast.makeText(getApplicationContext(), "Check your Internet Connectivity", Toast.LENGTH_SHORT).show();
         }
-        else
+    }
+
+    private void LocationPermission()
+    {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
         {
-            Toast.makeText(getApplicationContext(),"Check your Internet Connectivity",Toast.LENGTH_SHORT).show();
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION))
+            {
+                ActivityCompat.requestPermissions(Initiate.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+            else
+                {
+                    ActivityCompat.requestPermissions(Initiate.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_LOCATION);
+                }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults)
+    {
+        switch (requestCode)
+        {
+            case MY_PERMISSIONS_REQUEST_LOCATION:
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+                    {
+                        startActivity(new Intent(Initiate.this, Dashboard.class));
+                        finish();
+                    }
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(),"Allow Location to Continue",Toast.LENGTH_SHORT).show();
+                }
         }
     }
 }
