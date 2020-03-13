0ea295e6a5e991eb4d9e1aa3933e59ba68122318
PvtSec <akssofts@gmail.com>
2020-03-13 21:36:40 +0530

    Inital Prefernces

--- app/src/main/java/agro/meteoro/wayanad/Preferences.java
+++ app/src/main/java/agro/meteoro/wayanad/Preferences.java
@@ -1,14 +1,46 @@
 package agro.meteoro.wayanad;
 
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.Locale;
 
-public class Preferences extends AppCompatActivity {
-
+public class Preferences extends AppCompatActivity
+{
+    ImageButton start;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preferences);
+    }
+
+    private void change(String localeCode)
+    {
+        Resources resources = getResources();
+        DisplayMetrics dm = resources.getDisplayMetrics();
+        Configuration config = resources.getConfiguration();
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.JELLY_BEAN_MR1)
+        {
+            config.setLocale(new Locale(localeCode.toLowerCase()));
+        }
+        else
+        {
+            config.locale = new Locale(localeCode.toLowerCase());
+        }
+        resources.updateConfiguration(config, dm);
+        Intent intent = getIntent();
+        overridePendingTransition(0, 0);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        finish();
+        overridePendingTransition(0, 0);
+        startActivity(intent);
     }
 }
